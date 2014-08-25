24f5d69edfa93d29af09610780d4ef24e4fda573
Brandon Aaskov <brandonaaskov@gmail.com>
2014-08-25 12:57:14 -0600

    First pass at the Lifelogging Camera
    It's capturing the streams as expected. I've been having trouble trying to get ffmpeg installed on the thing, but if I use it locally on my mac it encodes the outputs quickly and just fine. Still working on encode_recordings.py so that it takes the file name in properly before making the CLI call.

---
+++ simple-button.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+button = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(button)
+    if input_state == False:
+        print('Button Pressed')
+        time.sleep(0.2)
